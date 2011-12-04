549d65da1d6ab379d76ea520c03c06bd96230153
Steven Koch <stvkoch@gmail.com>
2011-12-04 16:16:56 +0000

    add simple mod_97_10

--- ISO7064Mod97_10.php
+++ ISO7064Mod97_10.php
@@ -1,40 +1,95 @@
 <?php
 /*
-require 'ISO7064Mod97_10.php';
+require 'ISO7064Mod97_10.php'
+$c = new ISO7064Mod97_10()
 
+$n = 107571
+$yourNum = $c->encode($n)
+var_dump($yourNum)
+//int(10757107)
+var_dump($c->verify($yourNum))
 
+$validNumber = '10757107'
+$invalidNumber = '10767107'
+var_dump($c->verify($validNumber))
+var_dump($c->verify($invalidNumber))
 
 */
 class ISO7064Mod97_10{
 	
 	function encode($str){
+		
+	  $c = $this->checkCode("${str}00");
+	  if($c == 0){
+	    return (int)"${str}00";
+	  }elseif($c < 10) {
+	    return (int)"${str}0${c}";
+	  }else{
+	    return (int)"${str}${c}";
+	  }
+	}
+	function verify($str){
+		return ((($this->computeCheck("$str")) % 97)==1);
+	}
+	
+	function checkCode($str){
+		//$this->computeCheck($str);
+		return ( 98 - ($this->computeCheck("$str")%97) );
+	}
+	
+	function computeCheck($str)
+	{
+		$ai=1;
+		$ch = ord($str[strlen($str)-1]) - 48;
+    if ($ch < 0 || $ch > 9) return false;
+		$check=$ch;
+		for($i=strlen($str)-2;$i>=0;$i--){
+			$ch = ord($str[$i]) - 48;
+	    if ($ch < 0 || $ch > 9) return false;
+			$ai=($ai*10)%97;
+			$check+= ($ai * ((int)$ch));
+		}
+		return $check;
+	}
+	
+	function getCheck($str){
+		return (int)substr("$str", strlen("$str")-2);
+	}
+	
+	function getData($str){
+		return (int)substr("$str", 0, strlen("$str")-2);
+	}
+}
+
+/*
+$codClient = 123456;
+
+require 'ISO7064Mod97_10.php';
+$checkISOMod97_10 = new Simple_ISO7064Mod97_10();
+
+$number = $checkISOMod97_10->encode($codClient);//12345676
+$getClientNumber = 12345675
+var_dump($checkISOMod97_10->verify($getClientNumber))
+
+*/
+class Simple_ISO7064Mod97_10{
+	
+	function encode($str){
 	  $c = $this->computeCheck($str);
 	  if($c == 0){
-	    return $str . "00";
+	    return (int)"${str}00";
 	  }elseif($c < 10) {
-	    return $str . "0" . $c;
+	    return (int)"${str}0${c}";
 	  }else{
-	    return $str . $c;
+	    return (int)"${str}${c}";
 	  }
 	}
 	function verify($str) {
-	  $check = 0;
-	  for ($i = 0; $i < strlen($str) -1; ++$i) {
-	    $ch = ord($str[$i]) - 48;
-	    if ($ch < 0 || $ch > 9) return false;
-	    $check = (($check + $ch) * 10) % 97;
-	  }
-	  $check += ord($str[strlen($str)-1])- 48;
-	  return ($check % 97) == 1;
+		return ((((int)$str) % 97)==1);
 	}
 	
-	function computeCheck($str) {
-		$check = 0;//var check = 0;
-		for($i=0; $i<strlen($str);$i++){
-			$ch = ord( $str[$i] ) - 48 ;
-			$check = (($check + $ch) * 10) % 97;
-		}
-	  return (98 - (($check*10) % 97)) % 97;
+	function computeCheck($str){
+		return ( 98 - (((int)$str*100)%97) )%97;
 	}
 	
 	function getCheck($str){
@@ -42,6 +97,6 @@ class ISO7064Mod97_10{
 	}
 	
 	function getData($str){
-		return substr($str, 0, strlen($str)-2);
+		return (int)substr($str, 0, strlen($str)-2);
 	}
 }
