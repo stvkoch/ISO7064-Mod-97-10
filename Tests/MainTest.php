3303e3ff1cfb7038dfe2e5a7d0e4c31b2138f79b
Steven Koch <stvkoch@gmail.com>
2012-10-01 16:23:15 -0700

    Updated Tests/MainTest.php

--- Tests/MainTest.php
+++ Tests/MainTest.php
@@ -1,3 +1,24 @@
-# How does it work?
+<?php
 
-Enter Text in Markdown format.
+  require 'ISO7064Mod97_10.php';
+
+class MainTest extends PHPUnit_Framework_TestCase
+{
+	public function testNumbers()
+    {
+      $c = new ISO7064Mod97_10();
+      $n = 107571;
+      $validNumber = '10757107';
+      $invalidNumber = '10767107';
+      
+
+      $this->assertEquals( $validNumber, $c->encode($n) );
+	  $this->assertNotEquals( $invalidNumber, $c->encode($n) );
+      
+      $this->assertTrue($c->verify($validNumber));
+      $this->assertFalse($c->verify($invalidNumber));
+      
+      
+      
+    }
+}
