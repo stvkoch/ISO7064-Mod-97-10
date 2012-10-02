26c311a768ee8a1c5051abdcb283cd28cfa9181d
Steven Koch <stvkoch@gmail.com>
2012-10-02 01:12:53 +0100

    tests

--- Tests/MainTest.php
+++ Tests/MainTest.php
@@ -1,11 +1,12 @@
 <?php
 
-  require '../ISO7064Mod97_10.php';
+require 'ISO7064Mod97_10.php';
 
 class MainTest extends PHPUnit_Framework_TestCase
 {
-	public function testNumbers()
+  public function testNumbers()
     {
+
       $c = new ISO7064Mod97_10();
       $n = 107571;
       $validNumber = '10757107';
@@ -13,12 +14,10 @@ class MainTest extends PHPUnit_Framework_TestCase
       
 
       $this->assertEquals( $validNumber, $c->encode($n) );
-	  $this->assertNotEquals( $invalidNumber, $c->encode($n) );
+      $this->assertNotEquals( $invalidNumber, $c->encode($n) );
       
-      $this->assertTrue($c->verify($validNumber));
-      $this->assertFalse($c->verify($invalidNumber));
-      
-      
+      $this->assertTrue( $c->verify($validNumber) );
+      $this->assertFalse( $c->verify($invalidNumber) );
       
     }
 }
